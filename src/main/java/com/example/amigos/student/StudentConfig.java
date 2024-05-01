e0602689cfed7585f6fceb3d57957baf12cec2d1
Elissa-DI <elissadusabe@gmail.com>
2024-05-01 10:26:46 +0200

    done with adding an entity in db

---
+++ src/main/java/com/example/amigos/student/StudentConfig.java
@@ -0,0 +1,42 @@
+package com.example.amigos.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student elissa = new Student(
+                    1L,
+                    "Elissa",
+                    "elissa.dusabe@gmail.com",
+                    LocalDate.of(2006, JULY, 5),
+                    18
+
+            );
+
+            Student alex = new Student(
+                    1L,
+                    "Alex",
+                    "alex01.dusabe@gmail.com",
+                    LocalDate.of(2000, SEPTEMBER, 5),
+                    21
+
+            );
+
+            //This is where we save our students
+            repository.saveAll(
+                    List.of(elissa, alex)
+            );
+        };
+    }
+}
--- src/main/java/com/example/amigos/student/StudentRepository.java
+++ src/main/java/com/example/amigos/student/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.example.amigos.student;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//This should be annotated as repository as this interface is responsible for data access
+@Repository
+public interface StudentRepository
+        extends JpaRepository<Student, Long> {
 }
